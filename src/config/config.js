1e24b9464d33c2736b5aa192dd0e9224b8a27a9d
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2021-02-22 22:32:09 +0700

    postgres heroku

--- src/config/config.js
+++ src/config/config.js
@@ -11,11 +11,26 @@
 //         idle: 10000
 //     }
 // };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "postgres",
+//     DB: "cilvies",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "cilvies",
+    HOST: "ec2-18-204-101-137.compute-1.amazonaws.com",
+    USER: "boxoqnxjopzzbb",
+    PASSWORD: "0ea8066d46d9a7fc6d6affa962606e7734dc5409bf9e9271e3443dd10d0db713",
+    DB: "dchirs4r410vfo",
     dialect: "postgres",
     pool: {
         max: 5,
