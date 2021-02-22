e0b5d762f974ce9c49cab10bdb660923ef447420
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2021-02-22 23:54:35 +0700

    server, config

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const dvdRoute = require('./src/routes/dvd')
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const db = require('./src/model');
 db.sequelize.sync({}).then(() => {
     console.log('Drop and re-sync db.');
--- src/config/config.js
+++ src/config/config.js
@@ -12,25 +12,11 @@
 //     }
 // };
 
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "postgres",
-//     PASSWORD: "postgres",
-//     DB: "cilvies",
-//     dialect: "postgres",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
-
 module.exports = {
-    HOST: "ec2-18-204-101-137.compute-1.amazonaws.com",
-    USER: "boxoqnxjopzzbb",
-    PASSWORD: "0ea8066d46d9a7fc6d6affa962606e7734dc5409bf9e9271e3443dd10d0db713",
-    DB: "dchirs4r410vfo",
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "postgres",
+    DB: "cilvies",
     dialect: "postgres",
     pool: {
         max: 5,
@@ -39,3 +25,17 @@ module.exports = {
         idle: 10000
     }
 };
+
+// module.exports = {
+//     HOST: "ec2-18-204-101-137.compute-1.amazonaws.com",
+//     USER: "boxoqnxjopzzbb",
+//     PASSWORD: "0ea8066d46d9a7fc6d6affa962606e7734dc5409bf9e9271e3443dd10d0db713",
+//     DB: "dchirs4r410vfo",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
